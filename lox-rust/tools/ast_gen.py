e52a2bdb9d4e740c50e9155cf06c55f6f6d6eabc
John Mumm <jtfmumm@gmail.com>
2023-06-22 12:30:18 +0200

    Add code gen

---
+++ lox-rust/tools/ast_gen.py
@@ -0,0 +1,58 @@
+import json
+
+class Field:
+	def __init__(self, name, typ):
+		self.name = name
+		self.type = typ
+
+	def __str__(self):
+		return self.name + ": " + self.type
+
+class Expr:
+	def __init__(self, name, fields):
+		self.name = name
+		self.fields = fields
+
+	def field_names(self):
+		return list(map(lambda x: x.name, self.fields))
+
+	def __str__(self):
+		fieldstrs = ""
+		for f in self.fields:
+			fieldstrs += str(f) + ', '
+		return self.name + ' -> ' + fieldstrs[:-2]
+
+def fieldize(s):
+	raw = s.split(' ')
+	return Field(raw[2].strip(), raw[1].strip())
+
+def parse_fields(s):
+	raw = s.split(',')
+	return list(map(fieldize, raw))
+
+def parse_expr(expr):
+	parts = expr.split(':')
+	name = parts[0].strip()
+	fields = parse_fields(parts[1])
+	return Expr(name, fields)
+
+with open('tools/ast_def.json', 'r') as f:
+	data = json.load(f)
+
+exprs = list(map(parse_expr, data))
+
+output = 'pub trait Expr {}\n\n'
+
+for e in exprs:
+	output += 'pub struct ' + e.name + ' {\n'
+	for f in e.fields:
+		output += '\t' + str(f) + ',\n'
+	output += '}\n\n'
+	output += 'impl ' + e.name + ' {\n'
+	output += '\tfn new('
+	output += ', '.join(map(str, e.fields)) + ') -> Self {\n'
+	output += '\t\t' + e.name + ' { '
+	output += ', '.join(e.field_names()) + ' }\n\t}\n}\n\n'
+	output += 'impl Expr for ' + e.name + ' {}\n\n'
+
+print(output)
